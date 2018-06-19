15789e50f45bcaa09341ed17852c5d3663a9de76
maijz128 <maijz128@gmail.com>
2018-06-19 11:41:32 +0800

    使用自定义线程池

--- src/main/java/xyz/maijz128/fileobserver/Application.java
+++ src/main/java/xyz/maijz128/fileobserver/Application.java
@@ -3,10 +3,14 @@ package xyz.maijz128.fileobserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Collections;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class Application {
 
 
